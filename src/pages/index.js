c370da2ee6ce298de2cba89c4036660d8e1740f7
mrsteveson <patrick.steveson727@gmail.com>
2020-02-13 17:29:15 -0600

    updating gallery photos

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,12 @@ import Helmet from 'react-helmet'
 import Layout from '../components/layout'
 import Gallery from '../components/Gallery'
 
-import thumb01 from '../assets/images/thumbs/01.jpg'
-import thumb02 from '../assets/images/thumbs/02.jpg'
-import thumb03 from '../assets/images/thumbs/03.jpg'
-import thumb04 from '../assets/images/thumbs/04.jpg'
-import thumb05 from '../assets/images/thumbs/05.jpg'
-import thumb06 from '../assets/images/thumbs/06.jpg'
+import riy from '../assets/images/projects/RIY.jpg'
+import cave from '../assets/images/projects/Cave.jpg'
+import treasure from '../assets/images/projects/treasure.jpg'
+import journal from '../assets/images/projects/journal.jpg'
+import lookbook from '../assets/images/projects/Lookbook.jpg'
+import techx from '../assets/images/projects/TechX.jpg'
 
 import full01 from '../assets/images/fulls/01.jpg'
 import full02 from '../assets/images/fulls/02.jpg'
@@ -32,12 +32,12 @@ import react from '../assets/images/badges/react.png'
 import sql from '../assets/images/badges/sql.png'
 
 const DEFAULT_IMAGES = [
-    { id: '1', source: full01, thumbnail: thumb01, caption: 'Photo 1', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
-    { id: '2', source: full02, thumbnail: thumb02, caption: 'Photo 2', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
-    { id: '3', source: full03, thumbnail: thumb03, caption: 'Photo 3', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
-    { id: '4', source: full04, thumbnail: thumb04, caption: 'Photo 4', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
-    { id: '5', source: full05, thumbnail: thumb05, caption: 'Photo 5', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
-    { id: '6', source: full06, thumbnail: thumb06, caption: 'Photo 6', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'}
+    { id: '1', source: full01, thumbnail: riy, caption: 'Review It Yourself', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
+    { id: '2', source: full02, thumbnail: cave, caption: 'Cave Escape', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
+    { id: '3', source: full03, thumbnail: treasure, caption: 'Treasure Hunt', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
+    { id: '4', source: full04, thumbnail: techx, caption: 'Tech Xchange', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
+    { id: '5', source: full05, thumbnail: journal, caption: 'LambdaFit', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
+    { id: '6', source: full06, thumbnail: lookbook, caption: 'The Lookbook', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'}
 ];
 
 class HomeIndex extends React.Component {
