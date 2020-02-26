a4da0fbb900896dd69ebd55175fceaf9eb02ad0e
mrsteveson <patrick.steveson727@gmail.com>
2020-02-26 11:04:38 -0600

    updating gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,6 +1,5 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import Carousel, { Modal, ModalGateway } from "react-images";
 
 class Gallery extends Component {
     constructor () {
@@ -11,27 +10,17 @@ class Gallery extends Component {
             selectedIndex: 0
         };
         
-        // this.toggleLightbox = this.toggleLightbox.bind(this);
     }
-    // toggleLightbox(selectedIndex) {
-    //     this.setState(state => ({
-    //         lightboxIsOpen: !state.lightboxIsOpen,
-    //         selectedIndex
-    //     }));
-    // }
+
     renderGallery (images) {
         if (!images) return;
 
         const gallery = images.map((obj, i) => {
+            console.log(obj)
             return (
                 <article className="6u 12u$(xsmall) work-item" key={i}>
                     <a
                         className="image fit thumb"
-                        href={obj.source}
-                        onClick={e => {
-                            e.preventDefault();
-                            this.toggleLightbox(i);
-                        }}
                     >
                         <img src={obj.thumbnail} alt='individual project'/>
                     </a>
@@ -54,18 +43,9 @@ class Gallery extends Component {
     }
     render () {
         const { images } = this.props;
-        const { selectedIndex, lightboxIsOpen } = this.state;
-
         return (
             <div>
                 {this.renderGallery(images)}
-                <ModalGateway>
-                    {lightboxIsOpen && (
-                        <Modal onClose={this.toggleLightbox}>
-                            <Carousel currentIndex={selectedIndex} views={images} />
-                        </Modal>
-                    )}
-                </ModalGateway>
             </div>
         );
     }
