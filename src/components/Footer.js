39c6338fa758ca8bab1b57470d51402324b68872
mrsteveson <patrick.steveson727@gmail.com>
2020-02-26 17:42:04 -0600

    update resume with pdf

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import resume from '../assets/images/Patrick_Steveson_Resume.pdf';
 
 class Footer extends React.Component {
     render() {
@@ -8,7 +9,7 @@ class Footer extends React.Component {
                     <ul className="icons">
                         <li><a href="https://linkedin.com/in/patricktsteveson" className="icon fa-linkedin"><span className="label">LinkedIn</span></a></li>
                         <li><a href="https://github.com/mrsteveson" className="icon fa-github"><span className="label">Github</span></a></li>
-                        <li><a href="https://docs.google.com/document/d/1NRi__5oozf6C2neaxn-M2lzMI7kIOIxbfcqkWhDJINw/edit?usp=sharing" className="icon fa-file"><span className="label">Resume</span></a></li>
+                        <li><a href={resume} className="icon fa-file"><span className="label">Resume</span></a></li>
                         <li><a href="mailto:patrick.steveson727@gmail.com" className="icon fa-envelope-o"><span className="label">Email</span></a></li>
                     </ul>
                     <ul className="copyright">
